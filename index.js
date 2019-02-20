75fcd206bd1a337d7559366ba92331a670745790
umbur <work1415@gmail.com>
2019-02-20 15:02:32 -0700

    Created GET endpoint to retrieve all the users from the userDb

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // code away!
+const postDb = require("./data/helpers/postDb");
+const userDb = require("./data/helpers/userDb");
+
+const express = require('express');
+const server = express();
+const helmet = require("helmet");
+
+server.use(express.json());
+server.use(helmet())
+
+// Users Endpoint
+server.get("/api/users", (req, res) => {
+    userDb
+      .get()
+      .then(users => res.status(200).json(users))
+      .catch(err => {
+          res.status(500).json({ error: 'The user information could not be retrieved.' })
+      })
+      
+  })
+  
+
+
+
+
+
+
+
+
+
+server.listen(5000, () => {
+    console.log('\n* Server Running on http://localhost:5000 *\n');
+  });
+   
