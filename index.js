ad6a1773512f0764cd92ebe3ef717b68dbc70899
umbur <work1415@gmail.com>
2019-02-20 15:13:14 -0700

    GET request can be used with specified id to get specific user

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ server.use(express.json());
 server.use(helmet())
 
 // Users Endpoint
+// Getting all the users
 server.get("/api/users", (req, res) => {
     userDb
       .get()
@@ -20,8 +21,22 @@ server.get("/api/users", (req, res) => {
       
   })
   
+// Getting the user by id
+ server.get('/api/users/:id', (req, res) => {
+     const id = req.params.id
 
-
+     userDb.getById(id)
+     .then(user => {
+        if(user){
+            res.status(200).json(user);
+        } else {
+            res.status(400).json({ error: 'The user with the  specified ID does not exist.' })
+        }
+     })
+     .catch(err =>
+        res.status(500).json({ error: 'The user information could not be retrieved.' })
+        )
+    })
 
 
 
