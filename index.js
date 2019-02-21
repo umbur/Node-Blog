0e08a8ec07ef8647a7e6853b64094eb56902f4f7
umbur <work1415@gmail.com>
2019-02-21 14:51:19 -0700

    Installed dependencies for deployment on heroku

--- index.js
+++ index.js
@@ -119,9 +119,10 @@ server.put('/api/users/:id', capitalCheck, (req, res) => {
 
 
 
-server.listen(5000, () => {
-    console.log('\n* Server Running on http://localhost:5000 *\n');
+  const port = process.env.PORT || 4000
+  server.listen(port, () => {
+    console.log(`\n*** Server Running on http:localhost:${port} ***\n`);
   });
-
+  
 
    
